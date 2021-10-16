c1afabeae2dd1467a2a27304f4c9e09e8e3c8e4e
Mondooo <shixiaocgq@126.com>
2021-10-16 11:16:13 -0700

    solution of lab xv6 lazy page allocation: Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,17 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13 || r_scause() == 15) {
+    // 13 Load page fault
+    // 15 Store/AMO page fault
+    uint64 a = PGROUNDDOWN(r_stval());
+    char *mem = kalloc();
+    memset(mem, 0, PGSIZE);
+    if(mappages(p->pagetable, a, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree(mem);
+      p->killed = 1;
+    }
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
