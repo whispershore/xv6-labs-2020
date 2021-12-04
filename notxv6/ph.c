65ea259a3eb6205b0e972e644aa19abe83d8acfb
Mondooo <shixiaocgq@126.com>
2021-12-04 02:30:02 -0800

    solution of lab MultiThreading: Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock;            // declare a lock
 
 double
 now()
@@ -51,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);       // acquire lock
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);     // release lock
   }
 }
 
@@ -99,6 +102,8 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  pthread_mutex_init(&lock, NULL); // initialize the lock
+
   pthread_t *tha;
   void *value;
   double t1, t0;
